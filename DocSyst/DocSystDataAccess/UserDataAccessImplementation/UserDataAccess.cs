ea12efb513b0f17d61c58152102c85664aca2ccb
Unknown <sarbildi@gmail.com>
2018-04-20 20:24:52 -0300

    [Green] -         public void CreateUserDataAccess_WithoutParameters_Ok - test passed

---
+++ DocSyst/DocSystDataAccess/UserDataAccessImplementation/UserDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using DocSystDataAccessInterface.UserDataAccessInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocSystDataAccess.UserDataAccessImplementation
+{
+    public class UserDataAccess : IUserDataAccess
+    {
+    }
+}
--- DocSyst/DocSystTest/DataAccessTest/UserDataAccessTest.cs
+++ DocSyst/DocSystTest/DataAccessTest/UserDataAccessTest.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocSystDataAccessInterface.UserDataAccessInterface;
+using DocSystDataAccess.UserDataAccessImplementation;
 
 namespace DocSystTest.DataAccessTest
 {
