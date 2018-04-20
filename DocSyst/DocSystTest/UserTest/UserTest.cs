662d6bb968e064424631aef934febd35c4a31e11
Unknown <sarbildi@gmail.com>
2018-04-20 19:35:53 -0300

    [Red] - CreateUser_WithoutParameters_Ok - don't compile

---
+++ DocSyst/DocSystTest/UserTest/UserTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocSystTest.UserTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void CreateUser_WithoutParameters_Ok()
+        {
+            User user = new User();
+
+            Assert.IsNotNull(user);
+        }
+    }
+}
