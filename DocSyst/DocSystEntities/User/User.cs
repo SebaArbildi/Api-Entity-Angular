33ab25d4f3ff0dedc2bef90bd1ba486b62da713a
Unknown <sarbildi@gmail.com>
2018-04-20 19:44:08 -0300

    [Green] - CreateUser_WithParameters_Ok - test passed

--- DocSyst/DocSystEntities/User/User.cs
+++ DocSyst/DocSystEntities/User/User.cs
@@ -8,6 +8,23 @@ namespace DocSystEntities.User
 {
     public class User
     {
+        private string v1;
+        private string v2;
+        private string v3;
+        private string v4;
+        private string v5;
+        private bool v6;
+
         public User() { }
+
+        public User(string v1, string v2, string v3, string v4, string v5, bool v6)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+        }
     }
 }
