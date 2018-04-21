9e72a3e433c97f49688e8b98efd35e0d9c9c62c4
Unknown <sarbildi@gmail.com>
2018-04-21 12:17:35 -0300

    [Red] - UserBusinessLogicTest doesn't compile.
    CreateUserBL_WithoutParameters_Ok, CreateUserBL_WithParameters_Ok

---
+++ DocSyst/DocSystTest/BusinessLogicTest/UserBusinessLogicTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocSystTest.BusinessLogicTest
+{
+    [TestClass]
+    public class UserBusinessLogicTest
+    {
+        [TestMethod]
+        public void CreateUserBL_WithoutParameters_Ok()
+        {
+            IUserBusinessLogic userBusinessLogic = new UserBusinessLogicTest();
+
+            Assert.IsNotNull(userBusinessLogic);
+        }
+
+        [TestMethod]
+        public void CreateUserBL_WithParameters_Ok()
+        {
+            IUserDataAccess userDataAccess = new IUserDataAccess();
+
+            IUserBusinessLogic userBusinessLogic = new UserBusinessLogicTest(userDataAccess);
+
+            Assert.IsNotNull(userBusinessLogic);
+        }
+    }
+}
