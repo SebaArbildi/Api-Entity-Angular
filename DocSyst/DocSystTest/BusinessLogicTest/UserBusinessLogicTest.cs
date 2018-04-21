34cef4151a011c46c466d14873774a5a33079553
Unknown <sarbildi@gmail.com>
2018-04-21 17:28:13 -0300

    [Red] - UserBusinessLogic delete modify tests - doesn't compile

--- DocSyst/DocSystTest/BusinessLogicTest/UserBusinessLogicTest.cs
+++ DocSyst/DocSystTest/BusinessLogicTest/UserBusinessLogicTest.cs
@@ -70,5 +70,67 @@ namespace DocSystTest.BusinessLogicTest
 
             userBusinessLogic.AddUser(newUser);
         }
+
+        [TestMethod]
+        public void DeleteUser_ExpectedParameters_Ok()
+        {
+            User newUser = Utils.CreateUserForTest();
+
+            mockUserDataAccess.Setup(b1 => b1.Add(newUser));
+
+            userBusinessLogic.DeleteUser(newUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DeleteUser_UserHasNullFields_ArgumentNullException()
+        {
+            User newUser = Utils.CreateUserForTest();
+            newUser.Name = null;
+
+            userBusinessLogic.DeleteUser(newUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DuplicateWaitObjectException))]
+        public void DeleteUser_UserAlreadyExists_DuplicateException()
+        {
+            User newUser = Utils.CreateUserForTest();
+
+            mockUserDataAccess.Setup(b1 => b1.Exists(newUser.Username)).Returns(true);
+
+            userBusinessLogic.DeleteUser(newUser);
+        }
+
+        [TestMethod]
+        public void ModifyUser_ExpectedParameters_Ok()
+        {
+            User newUser = Utils.CreateUserForTest();
+
+            mockUserDataAccess.Setup(b1 => b1.Add(newUser));
+
+            userBusinessLogic.ModifyUser(newUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ModifyUser_UserHasNullFields_ArgumentNullException()
+        {
+            User newUser = Utils.CreateUserForTest();
+            newUser.Name = null;
+
+            userBusinessLogic.ModifyUser(newUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DuplicateWaitObjectException))]
+        public void ModifyUser_UserAlreadyExists_DuplicateException()
+        {
+            User newUser = Utils.CreateUserForTest();
+
+            mockUserDataAccess.Setup(b1 => b1.Exists(newUser.Username)).Returns(true);
+
+            userBusinessLogic.ModifyUser(newUser);
+        }
     }
 }
