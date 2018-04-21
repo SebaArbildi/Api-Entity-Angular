f9b74a339fc23d5cb4dba38b1e9ffcb1a78be2f7
Unknown <sarbildi@gmail.com>
2018-04-20 22:28:43 -0300

    [Yellow] - AddUserToDb_ExpectedParameters_Ok - Compile but test fail

--- DocSyst/DocSystDataAccess/UserDataAccessImplementation/UserDataAccess.cs
+++ DocSyst/DocSystDataAccess/UserDataAccessImplementation/UserDataAccess.cs
@@ -1,9 +1,19 @@
-﻿using DocSystDataAccessInterface.UserDataAccessInterface;
+﻿using System;
+using DocSystDataAccessInterface.UserDataAccessInterface;
+using DocSystEntities.User;
 
 namespace DocSystDataAccess.UserDataAccessImplementation
 {
     public class UserDataAccess : IUserDataAccess
     {
-        
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(string username)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
