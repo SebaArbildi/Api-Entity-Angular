d1d9cb1bfaac4c6c28672a7ec5744969cdc51d4b
Unknown <sarbildi@gmail.com>
2018-04-22 09:16:28 -0300

    [Red] - GetUser_UserExists_Ok - doesn't compile

--- DocSyst/DocSystTest/ApiTest/UserApiTest.cs
+++ DocSyst/DocSystTest/ApiTest/UserApiTest.cs
@@ -8,12 +8,15 @@ using DocSystDataAccess.UserDataAccessImplementation;
 using DocSystDataAccessInterface.UserDataAccessInterface;
 using DocSystBusinessLogicImplementation.UserBusinessLogicImplementation;
 using DocSystWebApi.Controllers;
+using DocSystEntities.User;
+using System.Web.Http;
 
 namespace DocSystTest.ApiTest
 {
     [TestClass]
     public class UserApiTest
     {
+        private User user;
         private Mock<IUserBusinessLogic> mockUserBusinessLogic;
         private UserController userController;
 
@@ -26,6 +29,7 @@ namespace DocSystTest.ApiTest
         [TestInitialize]
         public void TestInitialize()
         {
+            user = Utils.CreateUserForTest();
             mockUserBusinessLogic = new Mock<IUserBusinessLogic>();
             userController = new UserController(mockUserBusinessLogic.Object);
         }
@@ -34,6 +38,13 @@ namespace DocSystTest.ApiTest
         public void CreateUserController_WithParameters_Ok()
         {
             Assert.IsNotNull(userController);
+        }
+
+        [TestMethod]
+        public void GetUser_UserExists_Ok()
+        {
+            mockUserBusinessLogic.Setup(b1 => b1.GetUser(user.Username)).Returns(user);
+            IHttpActionResult statusObtained = userController.Get(user.Username);
         }
     }
 }
