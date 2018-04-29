aad6cfe1197702828636b8e9b5f9d997958a0777
Unknown <sarbildi@gmail.com>
2018-04-29 18:10:54 -0300

    [Red] - AuthorizationBusinessLogicTest - doesn't compile

---
+++ DocSyst/DocSystBusinessLogicImplementation/AuthorizationBusinessLogicImplementation/AuthorizationBusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DocSystBusinessLogicInterface.AuthorizationBusinessLogicInterface;
+
+namespace DocSystBusinessLogicImplementation.AuthorizationBusinessLogicImplementation
+{
+    public class AuthorizationBusinessLogic : IAuthorizationBusinessLogic
+    {
+        public bool IsAdmin(Guid token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAValidToken(Guid token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DocSyst/DocSystTest/BusinessLogicTest/AuthorizationBusinessLogicTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocSystDataAccessInterface.UserDataAccessInterface;
+using Moq;
+using DocSystEntities.User;
+using DocSystBusinessLogicInterface.AuthorizationBusinessLogicInterface;
+
+namespace DocSystTest.BusinessLogicTest
+{
+    [TestClass]
+    public class AuthorizationBusinessLogicTest
+    {
+
+        private Mock<IUserDataAccess> mockUserDataAccess;
+        private IAuthorizationBusinessLogic authorizationBusinessLogic;
+        private User user;
+
+        [TestCleanup]
+        public void CleanDataBase()
+        {
+            Utils.DeleteBd();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mockUserDataAccess = new Mock<IUserDataAccess>();
+            authorizationBusinessLogic = new authorizationBusinessLogic(mockUserDataAccess.Object);
+            user = Utils.CreateUserForTest();
+        }
+
+        [TestMethod]
+        public void TokenIsValid_validToken_Ok()
+        {
+
+        }
+
+        [TestMethod]
+        public void TokenIsValid_notValidToken_throwException()
+        {
+
+        }
+
+        [TestMethod]
+        public void UserIsAdmin_adminUser_Ok()
+        {
+
+        }
+
+        [TestMethod]
+        public void UserIsAdmin_notAdminUser_throwException()
+        {
+
+        }
+    }
+}
