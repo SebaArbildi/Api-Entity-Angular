2332a65f9d3414b7cd232b0cdcb304d89b674e57
Unknown <sarbildi@gmail.com>
2018-05-01 18:57:42 -0300

    [green] - CreateStyleClass_WithParameters_Ok - tests passed

---
+++ DocSyst/DocSystEntities/StyleStructure/StyleClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocSystEntities.StyleStructure
+{
+    public class StyleClass
+    {
+        private string name;
+        private Style style;
+
+        public StyleClass() { }
+
+        public StyleClass(string name, Style style)
+        {
+            this.Name = name;
+            this.Style = style;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Style Style
+        {
+            get
+            {
+                return style;
+            }
+
+            set
+            {
+                style = value;
+            }
+        }
+    }
+}
