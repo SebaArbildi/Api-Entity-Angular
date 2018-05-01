009005efc88d628d52d5aec0d779f802767833a5
Unknown <sarbildi@gmail.com>
2018-05-01 18:50:31 -0300

    [Green] - tests passed

---
+++ DocSyst/DocSystEntities/StyleStructure/Style.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocSystEntities.StyleStructure
+{
+    public class Style
+    {
+        private string name;
+        private SpecificStyle implementation;
+
+        public Style() { }
+
+        public Style(string name, SpecificStyle implementation)
+        {
+            this.Name = name;
+            this.Implementation = implementation;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public SpecificStyle Implementation
+        {
+            get
+            {
+                return implementation;
+            }
+
+            set
+            {
+                implementation = value;
+            }
+        }
+    }
+}
