cf6a1402f33d3b250420202ba523eee7a7b7e513
Unknown <sarbildi@gmail.com>
2018-05-01 18:37:44 -0300

    [Red] - CreateSpecificStyle_WithParameters_Ok - test fail

---
+++ DocSyst/DocSystTest/EntitiesTest/StyleStructure/SpecificStyleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocSystTest.EntitiesTest.StyleStructure
+{
+    [TestClass]
+    public class SpecificStyleTest
+    {
+        [TestMethod]
+        public void CreateSpecificStyle_WithoutParameters_Ok()
+        {
+            SpecificStyleTest specificStyle = new SpecificStyleTest();
+        }
+
+        [TestMethod]
+        public void CreateSpecificStyle_WithParameters_Ok()
+        {
+            SpecificStyleTest specificStyle = new SpecificStyle("name", "<html><body>{0}</body></html>");
+        }
+    }
+}
