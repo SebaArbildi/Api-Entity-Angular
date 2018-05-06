44a8a32e3281b78b038b1a3694645f740d81cd62
Unknown <sarbildi@gmail.com>
2018-05-06 13:04:36 -0300

    [Yellow] - SpecificStyleDataAccess - test failed

---
+++ DocSyst/DocSystDataAccessImplementation/StyleStructureDataAccessImplementation/SpecificStyleDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using DocSystDataAccessInterface.StyleStructureDataAccessInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocSystEntities.StyleStructure;
+
+namespace DocSystDataAccessImplementation.StyleStructureDataAccess
+{
+    public class SpecificStyleDataAccess : ISpecificStyleDataAccess
+    {
+        public void Add(SpecificStyle specificStyle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SpecificStyle> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SpecificStyle Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(SpecificStyle specificStyle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
