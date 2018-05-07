e8e937fff7af2c7a992165848872151d3c737d3c
Unknown <sarbildi@gmail.com>
2018-05-06 22:22:32 -0300

    [Yellow] - FormatDataAccess tests failed

---
+++ DocSyst/DocSystDataAccessImplementation/StyleStructureDataAccessImplementation/FormatDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using DocSystDataAccessInterface.StyleStructureDataAccessInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocSystEntities.StyleStructure;
+
+namespace DocSystDataAccessImplementation.StyleStructureDataAccessImplementation
+{
+    public class FormatDataAccess : IFormatDataAccess
+    {
+        public void Add(Format format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Format> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Format Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(Format format)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
