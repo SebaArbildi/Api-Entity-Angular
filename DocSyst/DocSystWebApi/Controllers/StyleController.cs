6dc883035b015d7cad959487e8ce3a64ad1ddec6
Unknown <sarbildi@gmail.com>
2018-05-09 12:07:35 -0300

    [Green] - StyleController tests passed

--- DocSyst/DocSystWebApi/Controllers/StyleController.cs
+++ DocSyst/DocSystWebApi/Controllers/StyleController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DocSystBusinessLogicInterface.AuthorizationBusinessLogicInterface;
+using DocSystBusinessLogicInterface.StyleStructureBusinessLogicInterface;
+using DocSystEntities.StyleStructure;
+using DocSystWebApi.Models.StyleStructureModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,37 +13,107 @@ namespace DocSystWebApi.Controllers
 {
     public class StyleController : ApiController
     {
+        private IStyleBusinessLogic StyleBusinessLogic { get; set; }
+        private IAuthorizationBusinessLogic AuthorizationBusinessLogic { get; set; }
+
+        public StyleController(IStyleBusinessLogic styleBusinessLogic, IAuthorizationBusinessLogic authorizationBusinessLogic)
+        {
+            StyleBusinessLogic = styleBusinessLogic;
+            AuthorizationBusinessLogic = authorizationBusinessLogic;
+        }
         // GET: api/Style
         public IHttpActionResult Get()
         {
-            return Ok();
+            try
+            {
+                Utils.IsAValidToken(Request, AuthorizationBusinessLogic);
+                Utils.HasAdminPermissions(Request, AuthorizationBusinessLogic);
+                IList<Style> styles = StyleBusinessLogic.Get();
+                IList<StyleModel> stylesModels = ConvertEntitiesToModels(styles);
+                return Ok(stylesModels);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         // GET: api/Style/5
-        public IHttpActionResult Get(int id)
+        public IHttpActionResult Get([FromUri]string name)
         {
-            return Ok();
+            try
+            {
+                Utils.IsAValidToken(Request, AuthorizationBusinessLogic);
+                Utils.HasAdminPermissions(Request, AuthorizationBusinessLogic);
+                Style style = StyleBusinessLogic.Get(name);
+                return Ok(StyleModel.ToModel(style));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         // POST: api/Style
-        public IHttpActionResult Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody]StyleModel styleModel)
         {
-            return Ok();
+            try
+            {
+                Utils.IsAValidToken(Request, AuthorizationBusinessLogic);
+                Utils.HasAdminPermissions(Request, AuthorizationBusinessLogic);
+                StyleBusinessLogic.Add(styleModel.ToEntity());
+                return Ok("Style added");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
 
         }
 
         // PUT: api/Style/5
-        public IHttpActionResult Put(int id, [FromBody]string value)
+        public IHttpActionResult Put([FromUri] string name, [FromBody]StyleModel styleModel)
         {
-            return Ok();
+            try
+            {
+                styleModel.Name = name;
+                Utils.IsAValidToken(Request, AuthorizationBusinessLogic);
+                Utils.HasAdminPermissions(Request, AuthorizationBusinessLogic);
+                StyleBusinessLogic.Modify(styleModel.ToEntity());
+                return Ok("Style Modified");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
 
         }
 
         // DELETE: api/Style/5
-        public IHttpActionResult Delete(int id)
+        public IHttpActionResult Delete([FromUri] String name)
         {
-            return Ok();
+            try
+            {
+                Utils.IsAValidToken(Request, AuthorizationBusinessLogic);
+                Utils.HasAdminPermissions(Request, AuthorizationBusinessLogic);
+                StyleBusinessLogic.Delete(name);
+                return Ok("Style deleted");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
 
+        }
+
+        private IList<StyleModel> ConvertEntitiesToModels(IList<Style> styles)
+        {
+            IList<StyleModel> stylesModels = new List<StyleModel>();
+            foreach (Style style in styles)
+            {
+                stylesModels.Add(StyleModel.ToModel(style));
+            }
+            return stylesModels;
         }
     }
 }
