979ab5b01078c4af3d1cef084571f0bcf9ef0ff1
Unknown <mathiasnunez13@gmail.com>
2018-06-17 20:34:48 -0300

    Little fix from merge

--- AngularApp/myApp/src/app/app.component.ts
+++ AngularApp/myApp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import  { UserService } from './users/user.service'; 
-import { DocumentService } from './documents/document.service';
 import  { UserService } from './services/user.service'; 
+import { DocumentService } from './documents/document.service';
 import  { LoginService } from './services/login.service'; 
 import  { StyleService } from './services/style.service'; 
 
@@ -9,7 +8,7 @@ import  { StyleService } from './services/style.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  providers: [UserService, LoginService, StyleService]
+  providers: [UserService, LoginService, StyleService, DocumentService]
 })
 export class AppComponent {
   title = 'Obligatorio';
